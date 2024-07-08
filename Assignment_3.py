a7a2e4777e957f906dfae05b9624fc53edc0dd07
Jonson Daggy <156325729+DaggyJ@users.noreply.github.com>
2024-07-09 00:17:07 +0300

    Update Assignment_3.py
    updated

--- Assignment_3.py
+++ Assignment_3.py
@@ -1,9 +1,13 @@
         #MATHS_OPARETOR THAT TAKES TWO NUMS
 #Definiton of numbers
 def Math_operations(num1, num2):
+    '''
+    math_operations function takes two integers parameters
+    uses lambda fuction to calculate sum, product, quotient and difference of the numbers
+    '''
     """"
-lambda arguments : expression
-lambda functions can take any number of arguments
+    lambda arguments : expression
+    lambda functions can take any number of arguments
     """
 #Lambda functions 
     mySum = lambda num1, num2 : num1 + num2
@@ -20,10 +24,13 @@ lambda functions can take any number of arguments
     "quotient" : myQuotient(num1, num2),
     "difference" : myDifference(num1, num2)
 }
-#Doctstring for math_operations
-result = Math_operations(32, 8)
 
-print("Sum: ", result["sum"])
-print("Product: ", result["product"])
-print("Quotient: ", result["quotient"])
-print("Difference: ", result["difference"])
+def main():
+    result = Math_operations(48, 12)
+    print("Sum: ", result["sum"])
+    print("Product: ", result["product"])
+    print("Quotient: ", result["quotient"])
+    print("Difference: ", result["difference"])
+
+if __name__ == '__main__':
+    main()
